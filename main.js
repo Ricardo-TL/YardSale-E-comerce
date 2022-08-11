d9c8daa83a61de84dfab436bebc8a1e3e364f5c0
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-11 01:30:56 -0500

    Se fusionó el carrito de compras y se arregló el problema de superposición... Tambien se modificaron los estilos para evitar novedades

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleShoppingCart);
+
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive')
@@ -13,12 +17,21 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   console.log('click')
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive')
+  }
+
   mobileMenu.classList.toggle('inactive')
 };
 
-// menuEmail.addEventListener("click", function(){toggleDesktopMenu(desktopMenu)});
-// menuHamIcon.addEventListener("click", function(){toggleDesktopMenu(mobileMenu)});
+function toggleShoppingCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
-// function toggleDesktopMenu(elemento){
-//     elemento.classList.toggle("inactive");
-// }
+  if (!isMobileMenuClosed) {
+    mobilMenu.classList.add('inactive')
+  }
+
+  aside.classList.toggle('inactive');
+}
