41ee679c3403fdb01f5d3cb2606bd4edcac0cb2e
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-12 00:04:00 -0500

    Se creó una funcion para renderizar de una mejor manera el codigo y para hallar un bug

--- main.js
+++ main.js
@@ -86,38 +86,42 @@ productList.push({
       </div>
     </div> */
 
-for (product of productList) {
-  const productCard = document.createElement('div');
-  productCard.classList.add('product-card');
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
 
-  const productImg = document.createElement('img');
-  productImg.setAttribute('src', product.image)
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
 
-  const productInfo = document.createElement('div');
-  productInfo.classList.add('product-Info');
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
 
-  const productInfoDiv = document.createElement('div');
-  const productPrice = document.createElement('p');
-  productPrice.innerText = '$' + product.price;
-  const productName = document.createElement('p')
-  productName.innerText = product.name;
+    const productInfoDiv = document.createElement('div');
 
-  productInfoDiv.appendChild(productPrice)
-  productInfoDiv.appendChild(productName)
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
 
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
 
-  const productInfoFigure = document.createElement('figure');
-  const productImgCart = document.createElement('img');
-  productImgCart.setAttribute('src', '/shopper/assets/icons/bt_add_to_cart.svg')
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
 
-  productInfoFigure.appendChild(productImgCart);
+    productInfoFigure.appendChild(productImgCart);
 
-  productInfo.appendChild(productInfoDiv);
-  productInfo.appendChild(productInfoFigure);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
 
   productCard.appendChild(productImg);
   productCard.appendChild(productInfo);
 
-
-  cardsContainer.appendChild(productCard)
+  cardsContainer.appendChild(productCard);
+  }
 }
+
+renderProducts(productList);
